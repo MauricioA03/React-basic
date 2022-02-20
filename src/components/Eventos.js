6d9cec01f765c1eca90ca9cacd27a1c71edc0181
MauricioA03 <mauricio.crispink@gmail.com>
2022-02-20 16:31:34 -0400

    React-9-Events and Binding

---
+++ src/components/Eventos.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class Eventos extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            contador: 0,
+        };
+
+        this.sumar = this.sumar.bind(this);
+        this.restar = this.restar.bind(this);
+    }
+
+    sumar(e) {
+        console.log("sumando");
+        console.log(this);
+        this.setState({
+            contador: this.state.contador + 1,
+        })
+    }
+
+    restar(e) {
+        console.log("restar");
+        console.log(this);
+        this.setState({
+            contador: this.state.contador - 1,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Eventos en Componentes de clase</h2>
+                <nav>
+                    <button onClick={this.sumar}>+</button>
+                    <button onClick={this.restar}>-</button>
+                </nav>
+                <h3>{this.state.contador}</h3>
+            </div>
+        );
+    }
+}
+
+export default Eventos;
