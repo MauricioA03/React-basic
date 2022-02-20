3b3f0fdc83a82d63ea8f01ee1ac74d71d44d1129
MauricioA03 <mauricio.crispink@gmail.com>
2022-02-19 21:17:14 -0400

    React-5-Properties

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-              Learn React
-          </a>
-      </header>
-        <section>
-            <Componente mdg={"hola soy un componente funcional expreasado desde una props"}/>
-        </section>
+        <header className="App-header">
+            <section>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </section>
+            <section>
+                <Componente mdg={"hola soy un componente funcional expreasado desde una props"}/>
+                <hr/>
+                <Propiedades
+                    cadena={"Esto es una cadena de texto"}
+                    numero={19}
+                    booleando={true}
+                    arreglo={[1, 2, 3]}
+                    objeto={{
+                        nombre: "Mau",
+                        correo: "mau@gmail.com",
+                    }}
+                    funcion={(num) => num * num}
+                    elementoReact={<i>Esto es un elemento react</i>}
+                    componenteReact={<Componente mdg={"Soy un componente pasado como props"}/>}
+                />
+            </section>
+        </header>
     </div>
   );
 }
---
+++ src/components/Propiedades.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function Propiedades(props) {
+    return (
+        <div>
+            <h2>{props.porDefecto}</h2>
+            <ul>
+                <li>{props.cadena}</li>
+                <li>{props.numero}</li>
+                <li>{props.booleando ? "true" : "false"}</li>
+                <li>{props.arreglo.join(", ")}</li>
+                <li>{props.objeto.nombre + " - " + props.objeto.correo}</li>
+                <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+                <li>{props.elementoReact}</li>
+                <li>{props.componenteReact}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Propiedades;
+
+Propiedades.defaultProps = {
+    porDefecto: "Las Props",
+}
+
+Propiedades.prototype = {
+    numero: PropTypes.number.isRequired,
+}
