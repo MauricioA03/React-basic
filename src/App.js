c939f17f3057c0726f75a10bd3e68b7051a5a258
MauricioA03 <mauricio.crispink@gmail.com>
2022-02-19 20:32:09 -0400

    React-4-Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Componente from "./components/Componente";
 
 function App() {
   return (
@@ -9,15 +10,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+              Learn React
+          </a>
       </header>
+        <section>
+            <Componente mdg={"hola soy un componente funcional expreasado desde una props"}/>
+        </section>
     </div>
   );
 }
---
+++ src/components/Componente.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Componente = (props) => <h2>{props.mdg}</h2>
+
+// function Componente(props) {
+//     return (
+//         <div>
+//             <h2>
+//                 {props.mdg}
+//             </h2>
+//         </div>
+//     );
+// }
+
+export default Componente;
